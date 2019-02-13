f623b9224198290b6716c96f51b61ae51440b0e3
philanthropic-seal <45774540+philanthropic-seal@users.noreply.github.com>
2019-02-13 12:44:38 -0600

    Add comments

--- src/com/mcfarevee/shopping/Cart.java
+++ src/com/mcfarevee/shopping/Cart.java
@@ -1,17 +1,38 @@
 package com.mcfarevee.shopping;
 
+//This class utilizes the methods in the 'groceries' package to construct a 'shopping cart' 
+//The cart keeps track of these items and their attributes
 
 import java.util.ArrayList;
 import com.mcfarevee.groceries.*;
 import com.mcfarevee.groceries.Package;
 
 public class Cart {
+  
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  //Initializes the array list which will hold items in the cart
   private ArrayList<Item> cart;
+  
+  //Records the current total price
   private int totalPrice;
+  
+  //Records the current total weights of each unit catagory
   private Weight[] weightList;
+  
+  //Records the current number of things in the cart
   private int numThings;
+  
+  //Records the current number of entries in the cart
   private int numEntries;
 
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+  
+  //Create a new cart
   public Cart() {
     this.cart = new ArrayList<Item>();
     this.totalPrice = 0;
@@ -21,17 +42,22 @@ public class Cart {
         new Weight(Unit.KILOGRAM, 0), new Weight(Unit.GRAM, 0)};
   }
 
+  // +---------+-----------------------------------------------------
+  // | Methods |
+  // +---------+
+  
+  //Prints the contents of the cart
   public void printContents() {
     for (Item item : this.cart) {
       System.out.println(item.toString());
     }
   }
 
+  //Adds an item to the cart
+  //This method also updates all the relevant fields which record attributes of the items in the cart
   public void addItem(Item item) {
     this.cart.add(item);
-
     this.totalPrice += item.getPrice();
-
     Unit itemUnit = item.getWeight().getUnit();
     if (itemUnit.equals(Unit.POUND)) {
       this.weightList[0].setAmount(this.weightList[0].getAmount() + item.getWeight().getAmount());
@@ -42,13 +68,11 @@ public class Cart {
     } else if (itemUnit.equals(Unit.GRAM)) {
       this.weightList[3].setAmount(this.weightList[3].getAmount() + item.getWeight().getAmount());
     }
-
     if (item instanceof ManyPackages) {
       this.numThings += ((ManyPackages) item).getCount();
     } else {
-      this.numThings++;
+      this.numThings++; 
     }
-
     this.numEntries++;
   }
 
@@ -102,15 +126,16 @@ public class Cart {
   }
   */
   
+  //Merges any like BulkItems or Package/ManyPackage items into a single item in the cart
   public void merge() { 
-    int amount = 0;
+    int amount = 0; //Fields for tracking combined amounts of the two items
     for (int x = 0; x < cart.size(); x++) { 
       Item i;
       Item j;
-      for(int y = x + 1; y < cart.size(); y++) {
+      for(int y = x + 1; y < cart.size(); y++) { //Iterate through every item on the list
         i = cart.get(x);
         j = cart.get(y);
-        if (i.getName().contentEquals(j.getName())) {
+        if (i.getName().contentEquals(j.getName())) { //Conditionals for every combination of Package/ManyPackage
           if (i instanceof Package) {
             amount = 1;
             if ((j instanceof Package) && (((Package) i).equals((Package) j))) {
@@ -143,7 +168,7 @@ public class Cart {
               y--;
               cart.set(x, new ManyPackages(((ManyPackages) i).getPackage(), amount));
             }
-          } else if (i instanceof BulkItem) {
+          } else if (i instanceof BulkItem) { //Conditional for BuikItem
             amount = ((BulkItem) i).getAmount();
             if ((j instanceof BulkItem)
                 && ((BulkItem) i).getFood().equals(((BulkItem) j).getFood())) {
@@ -159,16 +184,17 @@ public class Cart {
     }
   }
 
-
-
+  //Return number of entries
   public int numEntries() {
     return this.numEntries;
   }
 
+  //Return number of things
   public int numThings() {
     return this.numThings;
   }
 
+  //Remove an item with the specific string
   public void remove(String name) {
     for (Item item : this.cart) {
       if (item.getName().contentEquals(name)) {
